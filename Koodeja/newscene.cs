63dd6d14bfe97cfa5b461905852015c54b369bdf
EetuTap <44426109+EetuTap@users.noreply.github.com>
2020-02-25 13:07:32 +0200

    Add files via upload

---
+++ Koodeja/newscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class newscene : MonoBehaviour
+{
+
+    public void SceneTransition(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("LoadScene");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+}
